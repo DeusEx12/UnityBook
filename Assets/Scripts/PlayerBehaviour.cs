305fec7a5710427ced055e74394c21af47cddc2a
DeusEx12 <alexander@kanet.ru>
2017-12-11 01:41:22 +0200

    summary
    sdfsdfsdf

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine; 
+
+/// <summary> 
+/// Responsible for moving the player automatically and  
+/// reciving input. 
+/// </summary> 
+[RequireComponent(typeof(Rigidbody))] 
+
+public class PlayerBehaviour : MonoBehaviour 
+{ 
+	/// <summary> 
+	/// A reference to the Rigidbody component 
+	/// </summary> 
+	private Rigidbody rb; 
+
+	[Tooltip("How fast the ball moves left/right")] 
+	public float dodgeSpeed = 5; 
+
+
+	[Tooltip("How fast the ball moves forwards automatically")] 
+	[Range(0, 10)] 
+	public float rollSpeed = 5; 
+
+	/// <summary> 
+	/// Use this for initialization 
+	/// </summary> 
+	void Start () 
+	{ 
+		// Get access to our Rigidbody component 
+		rb = GetComponent<Rigidbody>(); 
+	} 
+
+	/// <summary> 
+	/// Update is called once per frame 
+	/// </summary> 
+	void Update () 
+	{ 
+		// Check if we're moving to the side 
+		var horizontalSpeed = Input.GetAxis("Horizontal") *  
+			dodgeSpeed; 
+
+		// Apply our auto-moving and movement forces 
+		rb.AddForce(horizontalSpeed, 0, rollSpeed); 
+	} 
+} 
